733d5457c225fb934bb020b5e370d10c7f36fece
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-30 22:20:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+   try {
+    // Use fs.promises.writeFile to create a new file with the given filename and content
+    await fs.promises.writeFile(fileName, fileContent, "utf-8");
+
+    // No need to return anything for this assignment
+  } catch (error) {
+    // Handle any errors that might occur during file creation
+    console.error("Error creating the file:", error.message);
+    throw error;
+  }
 };
 
 module.exports = { writeFile };
